50f9b28472f8b643fee398056aabc4fcda935d20
dannydiazrm-prog <dannydiazrm-prog@users.noreply.github.com>
2026-03-04 02:01:37 +0000

    deploy: ae39a96ca21fb289aabd25a943738a4c8bcbbb24

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1326894877"
+    serviceWorkerVersion: "1259611964"
   }
 });
