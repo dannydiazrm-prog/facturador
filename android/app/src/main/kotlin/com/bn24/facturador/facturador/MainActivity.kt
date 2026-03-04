b55de2837a630335903060428793738a4fe7c92e
Julio Prieto <dannydiazrm@gmail.com>
2026-03-04 10:32:27 +0000

    edge to edge android

--- android/app/src/main/kotlin/com/bn24/facturador/facturador/MainActivity.kt
+++ android/app/src/main/kotlin/com/bn24/facturador/facturador/MainActivity.kt
@@ -1,5 +1,23 @@
 package com.bn24.facturador.facturador
 
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            )
+        }
+    }
+}
